1fad14a0035c86129803dc4a894d67674d325ad3
Dannus90 <persson.daniel.1990@gmail.com>
2020-07-16 23:32:23 +0200

    Initial Commit

---
+++ Models/Command.cs
@@ -0,0 +1,9 @@
+using System.Reflection;
+namespace Commander.Models {
+    public class Commander {
+        public int Id { get; set; }
+        public string Howto { get; set; }
+        public string Line { get; set; }
+        public string Platform { get; set; }
+    }
+}
