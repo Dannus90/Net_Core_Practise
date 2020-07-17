a556fe2ae5ff665d162bd03e11583068844e78ba
Dannus90 <persson.daniel.1990@gmail.com>
2020-07-17 02:29:55 +0200

    update CommandsController

--- Controllers/CommandsController.cs
+++ Controllers/CommandsController.cs
@@ -1,29 +1,40 @@
+using System.Reflection;
 using System.Collections.Generic;
 using Commander.Data;
 using Commander.Models;
 using Microsoft.AspNetCore.Mvc;
 
-namespace Commander.Controllers {
+namespace Commander.Controllers
+{
 
-    [Route ("api/commands")]
+    [Route("api/commands")]
     [ApiController]
-    public class CommandsController : ControllerBase {
+    public class CommandsController : ControllerBase
+    {
+        private readonly ICommanderRepo _repository;
 
-        private readonly MockCommanderRepo _repository = new MockCommanderRepo ();
+        public CommandsController(ICommanderRepo repository)
+        {
+            _repository = repository;
+        }
+
+        // private readonly MockCommanderRepo _repository = new MockCommanderRepo();
         //Get api/commands
 
         [HttpGet]
-        public ActionResult<IEnumerable<Command>> GetAllCommands () {
-            var commandItems = _repository.GetAppCommands ();
+        public ActionResult<IEnumerable<Command>> GetAllCommands()
+        {
+            var commandItems = _repository.GetAppCommands();
 
-            return Ok (commandItems);
+            return Ok(commandItems);
         }
 
         //Get  api/commands/{id}
-        [HttpGet ("{id}")]
-        public ActionResult<Command> GetCommandById (int id) {
-            var commandItem = _repository.GetCommandById (id);
-            return Ok (commandItem);
+        [HttpGet("{id}")]
+        public ActionResult<Command> GetCommandById(int id)
+        {
+            var commandItem = _repository.GetCommandById(id);
+            return Ok(commandItem);
         }
     }
 }
